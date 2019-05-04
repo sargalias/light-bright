5115c60337fea95f4b0781a133f913dc4ab90c93
Spyros Argalias <sargalias@gmail.com>
2019-05-04 23:12:15 +0100

    Config: Add Jest (and configure it).

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+};
