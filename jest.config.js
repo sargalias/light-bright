bf34a799802bc7b2b5a1d516f32c7c58d67a72a2
Spyros Argalias <sargalias@gmail.com>
2019-05-05 16:30:01 +0100

    Config: Add placeholder for jest resolver.
    Needed to match the webpack resolve: { alias } configuration.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss)$': 'identity-obj-proxy',
+
+    //
+    /*
+      webpack alias options
+      see https://jestjs.io/docs/en/webpack.html#configuring-jest-to-find-our-files
+      e.g. '^components/(.*)$': '<rootDir>/src/components/$1'
+    */
   },
   coverageThreshold: {
     global: {
