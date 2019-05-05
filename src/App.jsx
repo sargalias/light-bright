ecc3a58ba57781b351f4388b71674e6c55e847ec
Spyros Argalias <sargalias@gmail.com>
2019-05-05 23:14:10 +0100

    Develop: Add pages folder for main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import MainContentHeader from 'components/MainContentHeader';
-import LightBrightGame from 'components/LightBrightGame';
-import Footer from 'components/Footer';
+import LightBrightPage from 'pages/LightBrightPage';
 
-const App = () => (
-  <main>
-    <MainContentHeader />
-    <LightBrightGame />
-    <Footer />
-  </main>
-);
+const App = () => <LightBrightPage />;
 
 export default App;
---
+++ src/pages/LightBrightPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContentHeader from 'components/MainContentHeader';
+import LightBrightGame from 'components/LightBrightGame';
+import Footer from 'components/Footer';
+
+const LightBrightPage = () => (
+  <main>
+    <MainContentHeader />
+    <LightBrightGame />
+    <Footer />
+  </main>
+);
+
+export default LightBrightPage;
