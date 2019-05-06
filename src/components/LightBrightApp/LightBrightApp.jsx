3687642cb7c9a53b30d785a13ed153505bb8f01d
Spyros Argalias <sargalias@gmail.com>
2019-05-06 17:13:20 +0100

    Develop: Restructure app and HTML outline.
    Footer is now outside main.
    Abstract main section with light bright game into its own
    wrapper module LightBrightApp.

---
+++ src/components/LightBrightApp/LightBrightApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MainContentHeader from 'components/MainContentHeader';
+import LightBrightGame from 'components/LightBrightGame';
+import Instructions from 'components/Instructions';
+import bemUtils from 'utilities/bemUtils';
+import styles from './LightBrightApp.scss';
+
+const b = bemUtils(styles);
+
+const LightBrightPage = ({ parentClass }) => (
+  <div className={b('', '', parentClass)}>
+    <MainContentHeader parentClass={b('mainContentHeader')} />
+    <LightBrightGame parentClass={b('lightBrightGame')} />
+    <Instructions parentClass={b('instructions')} />
+  </div>
+);
+
+export default LightBrightPage;
--- src/pages/LightBrightPage/LightBrightPage.jsx
+++ src/pages/LightBrightPage/LightBrightPage.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import MainContentHeader from 'components/MainContentHeader';
-import LightBrightGame from 'components/LightBrightGame';
-import Instructions from 'components/Instructions';
-import Footer from 'components/Footer';
+import LightBrightApp from 'components/LightBrightApp';
+import Footer from 'layout/Footer';
 import bemUtils from 'utilities/bemUtils';
 import styles from './LightBrightPage.scss';
 
 const b = bemUtils(styles);
 
 const LightBrightPage = () => (
-  <main className={b()}>
-    <MainContentHeader parentClass={b('mainContentHeader')} />
-    <LightBrightGame parentClass={b('lightBrightGame')} />
-    <Instructions parentClass={b('instructions')} />
+  <>
+    <main className={b()}>
+      <LightBrightApp parentClass={b('lightBrightApp')} />
+    </main>
     <Footer />
-  </main>
+  </>
 );
 
 export default LightBrightPage;
