d893f5de18c084c7d6dbd7f1429566342108bc6e
Spyros Argalias <sargalias@gmail.com>
2019-05-06 18:50:46 +0100

    Develop: Add additional styling.

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -6,19 +6,21 @@ const b = bemUtils(styles);
 
 const Footer = ({ parentClass }) => (
   <div className={b('', '', parentClass)}>
-    <p className={b('text')}>
-      &copy; Spyros Argalias
-      {new Date().getFullYear()}
-    </p>
-    <p className={b('text')}>
-      <a
-        href="https://github.com/sargalias/light-bright"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View code
-      </a>
-    </p>
+    <div className={b('container')}>
+      <p className={b('text')}>
+        &copy; Spyros Argalias
+        {new Date().getFullYear()}
+      </p>
+      <p className={b('text')}>
+        <a
+          href="https://github.com/sargalias/light-bright"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View code
+        </a>
+      </p>
+    </div>
   </div>
 );
 
