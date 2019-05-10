b2112065d93426ac65d4d505a162f09358f54f05
Spyros Argalias <sargalias@gmail.com>
2019-05-10 23:24:53 +0100

    Develop: Make Cells <button> for accessibility.

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Cell.scss';
 
 const Cell = ({ parentClass, index, handleClick, hue }) => (
-  <div
+  <button
+    type="button"
     className={`${styles.Cell} ${hue ? styles.Cell___isOn : ''} ${parentClass ||
       ''}`}
     data-testid={index}
