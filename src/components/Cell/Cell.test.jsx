a9f487c566378f57fe26c2d98029c35ee5b50c2a
Spyros Argalias <sargalias@gmail.com>
2019-05-10 19:31:32 +0100

    Develop: Add test for Cell onClick functionality.

--- src/components/Cell/Cell.test.jsx
+++ src/components/Cell/Cell.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import Cell from './Cell';
 
 afterEach(cleanup);
@@ -11,4 +11,16 @@ test('Cell should have correct styling when color prop exists', () => {
   expect(cellNode.classList.contains('Cell')).toBe(true);
   expect(cellNode.classList.contains('Cell___isOn')).toBe(true);
   // TODO: jsdom can't test for CSS custom properties. Use a different testing environment
+});
+
+test('Cell should fire its handleClick prop when clicked', () => {
+  const mockHandleClick = jest.fn();
+  const { getByTestId } = render(
+    <Cell index="1" handleClick={mockHandleClick} />,
+  );
+  const cellNode = getByTestId('1');
+
+  fireEvent.click(cellNode);
+
+  expect(mockHandleClick).toHaveBeenCalledTimes(1);
 });
