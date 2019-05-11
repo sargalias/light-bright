9c9a387a8c5239418dc4e6b4d0803f8e372c3714
Spyros Argalias <sargalias@gmail.com>
2019-05-11 13:47:58 +0100

    Develop: Disable ESLint rules.
    eslint/no-loops:
      Kept getting in the way of tests where I need to test multiple
      cases.
    
    eslint/no-plusplus
      i++ statement for use in loops as noted above.

--- src/logic/randomHue.test.js
+++ src/logic/randomHue.test.js
@@ -2,7 +2,6 @@ import randomHue from './randomHue';
 
 describe('randomHue', () => {
   test('should generate a hue value between 0 and 359', () => {
-    // eslint-disable-next-line
     for (let i = 0; i < 1000; i++) {
       expect(randomHue()).toBeGreaterThanOrEqual(0);
       expect(randomHue()).toBeLessThan(360);
@@ -11,7 +10,6 @@ describe('randomHue', () => {
 
   test('should generate numbers randomly', () => {
     const numberSet = new Set();
-    // eslint-disable-next-line
     for (let i = 0; i < 1000; i++) {
       numberSet.add(randomHue());
     }
