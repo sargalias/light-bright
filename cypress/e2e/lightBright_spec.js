57a2674d134c29a672f75e48715adbf3a7468bb2
Spyros Argalias <sargalias@gmail.com>
2019-05-12 02:11:51 +0100

    drag-same-color: Add E2E test for dragging having the same color.

--- cypress/e2e/lightBright_spec.js
+++ cypress/e2e/lightBright_spec.js
@@ -80,4 +80,67 @@ describe('drag functionality', () => {
       .trigger('mouseover')
       .then(notHaveBackgroundImage);
   });
+
+  it.only('should color all Cells the same as the first cell', () => {
+    let firstBackgroundImage;
+
+    const getCellBackgroundImage = jCell =>
+      getComputedStyle(jCell[0]).backgroundImage;
+
+    const assertSameBackgroundImage = (image1, image2) =>
+      expect(image1).eq(image2);
+
+    const assertNotSameBackgroundImage = (image1, image2) =>
+      expect(image1).not.eq(image2);
+
+    cy.visit('/');
+
+    // click to begin drag
+    cy.getByTestId('30')
+      .trigger('mousedown')
+      .then(jCell => {
+        firstBackgroundImage = getCellBackgroundImage(jCell);
+      });
+
+    cy.getByTestId('31')
+      .trigger('mouseover')
+      .then(jCell => {
+        const backgroundImage = getCellBackgroundImage(jCell);
+        assertSameBackgroundImage(firstBackgroundImage, backgroundImage);
+      });
+    cy.getByTestId('32')
+      .trigger('mouseover')
+      .then(jCell => {
+        const backgroundImage = getCellBackgroundImage(jCell);
+        assertSameBackgroundImage(firstBackgroundImage, backgroundImage);
+      });
+
+    // end drag
+    cy.getByTestId('33').trigger('mouseup');
+
+    // begin new drag
+    cy.getByTestId('30')
+      .trigger('mousedown')
+      .then(jCell => {
+        const backgroundImage = getCellBackgroundImage(jCell);
+        assertNotSameBackgroundImage(firstBackgroundImage, backgroundImage);
+        firstBackgroundImage = getCellBackgroundImage(jCell);
+      });
+
+    cy.getByTestId('31')
+      .trigger('mouseover')
+      .then(jCell => {
+        const backgroundImage = getCellBackgroundImage(jCell);
+        assertSameBackgroundImage(firstBackgroundImage, backgroundImage);
+      });
+    cy.getByTestId('32')
+      .trigger('mouseover')
+      .then(jCell => {
+        const backgroundImage = getCellBackgroundImage(jCell);
+        assertSameBackgroundImage(firstBackgroundImage, backgroundImage);
+      });
+
+    // end drag
+    cy.getByTestId('33').trigger('mouseup');
+  });
 });
