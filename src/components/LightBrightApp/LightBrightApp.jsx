4224007f532749a8b6abbbda8fb2d7e684b1793f
Spyros Argalias <sargalias@gmail.com>
2019-05-12 14:17:50 +0100

    Refactor: Refactor logic from LightBrightApp in to a custom hook.

--- src/components/LightBrightApp/LightBrightApp.jsx
+++ src/components/LightBrightApp/LightBrightApp.jsx
@@ -1,70 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import MainContentHeader from 'components/MainContentHeader';
 import LightBrightGame from 'components/LightBrightGame';
 import Instructions from 'components/Instructions';
-import randomHue from 'logic/randomHue';
 import bemUtils from 'utilities/bemUtils';
+import useLightBrightAppLogic from 'logic/useLightBrightApp';
 import styles from './LightBrightApp.scss';
 
 const b = bemUtils(styles);
 
 const LightBrightApp = ({ parentClass, numCells }) => {
-  const [cells, setCells] = useState(Array.from({ length: numCells }));
-  const [isDragging, setIsDragging] = useState(false);
-  const [currentColor, setCurrentColor] = useState();
-
-  const getCellIndex = useCallback(
-    e => Number(e.target.getAttribute('data-cell')),
-    [],
-  );
-
-  const lightCells = useCallback(
-    (oldCells, cellIndexToColor, color) =>
-      Array.from(oldCells, (cell, i) =>
-        i === cellIndexToColor ? color : cell,
-      ),
-    [],
-  );
-
-  const handleCellClick = useCallback(
-    e => {
-      const cellIndex = getCellIndex(e);
-      const newColor = randomHue();
-      const newCells = lightCells(cells, cellIndex, newColor);
-      setCurrentColor(newColor);
-      setIsDragging(true);
-      setCells(newCells);
-    },
-    [getCellIndex, cells, lightCells],
-  );
-
-  const handleCellMouseOver = useCallback(
-    e => {
-      if (isDragging) {
-        const cellIndex = getCellIndex(e);
-        const newCells = lightCells(cells, cellIndex, currentColor);
-        setCells(newCells);
-      }
-    },
-    [cells, isDragging, currentColor, getCellIndex, lightCells],
-  );
-
-  const handleCellKeyPress = useCallback(
-    e => {
-      if (['Enter', ' '].includes(e.key)) {
-        const cellIndex = getCellIndex(e);
-        const newColor = randomHue();
-        const newCells = lightCells(cells, cellIndex, newColor);
-        setCells(newCells);
-      }
-    },
-    [cells, getCellIndex, lightCells],
-  );
-
-  const handleBoardMouseUp = useCallback(() => {
-    setIsDragging(false);
-    setCurrentColor(undefined);
-  }, [setIsDragging]);
+  const {
+    cells,
+    handleBoardMouseUp,
+    handleCellKeyPress,
+    handleCellMouseOver,
+    handleCellClick,
+  } = useLightBrightAppLogic(numCells);
 
   return (
     <div className={b('', '', parentClass)}>
---
+++ src/logic/useLightBrightApp.jsx
@@ -0,0 +1,71 @@
+import { useState, useCallback } from 'react';
+import randomHue from 'logic/randomHue';
+
+const LightBrightApp = numCells => {
+  const [cells, setCells] = useState(Array.from({ length: numCells }));
+  const [isDragging, setIsDragging] = useState(false);
+  const [currentColor, setCurrentColor] = useState();
+
+  const getCellIndex = useCallback(
+    e => Number(e.target.getAttribute('data-cell')),
+    [],
+  );
+
+  const lightCells = useCallback(
+    (oldCells, cellIndexToColor, color) =>
+      Array.from(oldCells, (cell, i) =>
+        i === cellIndexToColor ? color : cell,
+      ),
+    [],
+  );
+
+  const handleCellClick = useCallback(
+    e => {
+      const cellIndex = getCellIndex(e);
+      const newColor = randomHue();
+      const newCells = lightCells(cells, cellIndex, newColor);
+      setCurrentColor(newColor);
+      setIsDragging(true);
+      setCells(newCells);
+    },
+    [getCellIndex, cells, lightCells],
+  );
+
+  const handleCellMouseOver = useCallback(
+    e => {
+      if (isDragging) {
+        const cellIndex = getCellIndex(e);
+        const newCells = lightCells(cells, cellIndex, currentColor);
+        setCells(newCells);
+      }
+    },
+    [cells, isDragging, currentColor, getCellIndex, lightCells],
+  );
+
+  const handleCellKeyPress = useCallback(
+    e => {
+      if (['Enter', ' '].includes(e.key)) {
+        const cellIndex = getCellIndex(e);
+        const newColor = randomHue();
+        const newCells = lightCells(cells, cellIndex, newColor);
+        setCells(newCells);
+      }
+    },
+    [cells, getCellIndex, lightCells],
+  );
+
+  const handleBoardMouseUp = useCallback(() => {
+    setIsDragging(false);
+    setCurrentColor(undefined);
+  }, [setIsDragging]);
+
+  return {
+    cells,
+    handleBoardMouseUp,
+    handleCellKeyPress,
+    handleCellMouseOver,
+    handleCellClick,
+  };
+};
+
+export default LightBrightApp;
