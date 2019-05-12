ed907c7245b763875dd27c9eb0686cadef37bb97
Spyros Argalias <sargalias@gmail.com>
2019-05-12 20:35:45 +0100

    colors-always-unique: Add functionality for colors-always-unique.

--- src/logic/randomHue.js
+++ src/logic/randomHue.js
@@ -1,5 +1,18 @@
+import arrayShuffle from 'array-shuffle';
+
+function* randomHueGen() {
+  const hues = arrayShuffle(Array.from({ length: 360 }, (el, index) => index));
+  let i = 0;
+  while (true) {
+    i %= 360;
+    yield hues[i++];
+  }
+}
+
+const it = randomHueGen();
+
 const randomHue = () => {
-  return Math.floor(Math.random() * 360);
+  return it.next().value;
 };
 
 export default randomHue;
