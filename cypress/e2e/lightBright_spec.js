e80a9d2c0dfda9b131c60c23837e92eafb3f1b44
Spyros Argalias <sargalias@gmail.com>
2019-05-13 18:26:24 +0100

    colors-always-unique: Add simple E2E test for colors-always-unique.
    E2E test clicks on 4 cells and checks that none of them have
    the same color.

--- cypress/e2e/lightBright_spec.js
+++ cypress/e2e/lightBright_spec.js
@@ -143,4 +143,40 @@ describe('drag functionality', () => {
     // end drag
     cy.getByTestId('33').trigger('mouseup');
   });
+
+  describe('colors', () => {
+    it('should be unique', () => {
+      const cellHues = [];
+      cy.visit('/');
+      cy.getByTestId('1')
+        .click()
+        .then(jCell => {
+          const cellHue = jCell[0].style.getPropertyValue('--hue');
+          cellHues.push(cellHue);
+        });
+      cy.getByTestId('2')
+        .click()
+        .then(jCell => {
+          const cellHue = jCell[0].style.getPropertyValue('--hue');
+          cellHues.push(cellHue);
+        });
+      cy.getByTestId('3')
+        .click()
+        .then(jCell => {
+          const cellHue = jCell[0].style.getPropertyValue('--hue');
+          cellHues.push(cellHue);
+        });
+      cy.getByTestId('4')
+        .click()
+        .then(jCell => {
+          const cellHue = jCell[0].style.getPropertyValue('--hue');
+          cellHues.push(cellHue);
+        });
+
+      // eslint-disable-next-line jest/valid-expect-in-promise, promise/prefer-await-to-then
+      cy.wrap(cellHues).then(cellHuesArr =>
+        expect(new Set(cellHuesArr).size).eq(cellHuesArr.length),
+      );
+    });
+  });
 });
