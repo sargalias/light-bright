b853c7e66c4fea45bbf5510ffe74646a86cbacf8
Spyros Argalias <sargalias@gmail.com>
2019-05-13 19:39:57 +0100

    Refactor: Remove useCallback calls.
    Apparently useCallback does not necessarily improve performance.
    Performance measuring needs to be done to determine whether
    useCallback is worth using and where.

--- src/logic/useLightBrightApp/useLightBrightApp.jsx
+++ src/logic/useLightBrightApp/useLightBrightApp.jsx
@@ -1,63 +1,46 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import randomHue from 'logic/randomHue'; // eslint-disable-line import/export
+
+const getCellIndex = e => Number(e.target.getAttribute('data-cell'));
+
+const lightCells = (oldCells, cellIndexToColor, color) =>
+  Array.from(oldCells, (cell, i) => (i === cellIndexToColor ? color : cell));
 
 const LightBrightApp = numCells => {
   const [cells, setCells] = useState(Array.from({ length: numCells }));
   const [isDragging, setIsDragging] = useState(false);
   const [currentColor, setCurrentColor] = useState();
 
-  const getCellIndex = useCallback(
-    e => Number(e.target.getAttribute('data-cell')),
-    [],
-  );
+  const handleCellClick = e => {
+    const cellIndex = getCellIndex(e);
+    const newColor = randomHue();
+    const newCells = lightCells(cells, cellIndex, newColor);
+    setCurrentColor(newColor);
+    setIsDragging(true);
+    setCells(newCells);
+  };
 
-  const lightCells = useCallback(
-    (oldCells, cellIndexToColor, color) =>
-      Array.from(oldCells, (cell, i) =>
-        i === cellIndexToColor ? color : cell,
-      ),
-    [],
-  );
+  const handleCellMouseOver = e => {
+    if (isDragging) {
+      const cellIndex = getCellIndex(e);
+      const newCells = lightCells(cells, cellIndex, currentColor);
+      setCells(newCells);
+    }
+  };
 
-  const handleCellClick = useCallback(
-    e => {
+  const handleCellKeyPress = e => {
+    if (['Enter', ' '].includes(e.key)) {
       const cellIndex = getCellIndex(e);
       const newColor = randomHue();
       const newCells = lightCells(cells, cellIndex, newColor);
-      setCurrentColor(newColor);
-      setIsDragging(true);
       setCells(newCells);
-    },
-    [getCellIndex, cells, lightCells],
-  );
+    }
+  };
 
-  const handleCellMouseOver = useCallback(
-    e => {
-      if (isDragging) {
-        const cellIndex = getCellIndex(e);
-        const newCells = lightCells(cells, cellIndex, currentColor);
-        setCells(newCells);
-      }
-    },
-    [cells, isDragging, currentColor, getCellIndex, lightCells],
-  );
-
-  const handleCellKeyPress = useCallback(
-    e => {
-      if (['Enter', ' '].includes(e.key)) {
-        const cellIndex = getCellIndex(e);
-        const newColor = randomHue();
-        const newCells = lightCells(cells, cellIndex, newColor);
-        setCells(newCells);
-      }
-    },
-    [cells, getCellIndex, lightCells],
-  );
-
-  const handleBoardMouseUp = useCallback(() => {
+  const handleBoardMouseUp = () => {
     setIsDragging(false);
     setCurrentColor(undefined);
-  }, [setIsDragging]);
+  };
 
   return {
     cells,
