e66cf984519a6552b098082639413db2f69571fa
Spyros Argalias <sargalias@gmail.com>
2019-05-20 20:55:26 +0100

    feat(sticky footer): Add scss for sticky footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LightBrightPage from 'pages/LightBrightPage';
 import 'base/_reset.scss';
+import 'base/_base.scss';
 import 'abstracts/_variables.scss';
 
 const App = () => <LightBrightPage />;
