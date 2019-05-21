a1f211d6a59d02b9525e04fef44ac3e0e17755d7
Spyros Argalias <sargalias@gmail.com>
2019-05-21 20:46:08 +0100

    feat(styling): Add general styling.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import bemUtils from 'bem-css-modules';
+import styles from './Button.scss';
+
+const b = bemUtils(styles);
 
 const Button = ({ text, id, handleClick }) => (
-  <button type="button" id={id} data-testid={id} onClick={handleClick}>
+  <button
+    type="button"
+    id={id}
+    data-testid={id}
+    onClick={handleClick}
+    className={b()}
+  >
     {text}
   </button>
 );
