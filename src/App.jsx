66f57cf6b7e0374310b2533f1e8a31bd91c1b182
Spyros Argalias <sargalias@gmail.com>
2019-05-21 23:10:43 +0100

    fix(tests): Import typeface-lato index.css directly
    This is needed to get Jest to work, otherwise it fails.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LightBrightPage from 'pages/LightBrightPage';
-import 'typeface-lato';
+import 'typeface-lato/index.css';
 import 'abstracts/_variables.scss';
 import 'base/_reset.scss';
 import 'base/_base.scss';
