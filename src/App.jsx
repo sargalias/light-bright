e66b53902f3f023aaf611163a5bd391109ea737c
Spyros Argalias <sargalias@gmail.com>
2019-05-21 22:28:07 +0100

    feat(styling): Use Lato for font-family.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import LightBrightPage from 'pages/LightBrightPage';
+import 'typeface-lato';
 import 'base/_reset.scss';
 import 'base/_base.scss';
+import 'base/_typography.scss';
 import 'abstracts/_variables.scss';
 
 const App = () => <LightBrightPage />;
